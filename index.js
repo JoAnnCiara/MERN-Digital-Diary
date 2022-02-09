eb96460e782b24dc8ecf9c6b5a56f6db79b46530
JoAnn Ciaramello <joannciaramello@gmail.com>
2022-02-09 14:32:08 -0500

    Add files via upload
    Updated through parts 1 and 2.  Will continue adding functionality.

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import cors from 'cors';
+
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://joannciara:Tennisgirl1@cluster0.ynuch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000; 
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
+const CONNECTION_URL = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+const PORT = process.env.PORT|| 5000;
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
+  
+
 
 
 
